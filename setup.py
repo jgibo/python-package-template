766540dd07c0dbd6a6627e47e62bb1f27258e72c
Jordan Gibbings <jordang@familyzone.com>
2021-10-13 23:54:18 +0800

    Initial

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = ""
+LONG_DESCRIPTION = ""
+
+setup(
+    name="package_name",
+    version=VERSION,
+    author="",
+    author_email="",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[]
+)
